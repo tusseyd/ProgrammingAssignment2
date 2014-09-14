bf82689dbc6db2edb5c065f1aae4e54238d5703a
David Tussey <davidtussey@gmail.com>
2014-09-14 13:02:50 -0400

    Homework assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,97 +1,76 @@
-## These two functions work togetther to allow for the creation
-## and retrieval of the inverse of a matrix.
+## These two functions, "makeVector" & "cacheSolve", work together to create and retrieve the inverse of a matrix.
 
-## The first function establishes a list of "setters" and "getters" associated
-## with the matrix 'x' which the second function uses.
+## The first function establishes a list of setter and getter functions associated with the matrix 'x' 
+## which the second function uses.
 
-## The function 'makeCacheMatrix' returns a list which contains four functions.
-## These fours functions provide methods (to use O-O) to set/get the matrix, and
-## to setinv and getinv. Use of "getters" and "setters" is a standard 
-## approach to object-oriented programming. This is how they are implemented
-## in R.
+## The function 'makeVector' returns a list which contains four functions.
+## These fours functions provide the ability to set/get the matrix, and set/get the inverse.
 
-## The second function 'cacheSolve' returns the inverse of the matrix 'x'.
-## If the inverse has already been computed, then it is retreived with the 
-## 'getinv' function.  If the matrix inverse has not already been computed, 
-## then the 'getinv' function will return a NULL value, and the remainderof
-## the function will be executed to generate the inverse
-## (with the 'solve' function), and then store that inverse
-## (with the 'setinv' function)
+## The function 'cacheSolve' returns the inverse of the matrix 'x'.
+## If the inverse has already been computed, it is retrieved from cache.
+## If the inverse has not already been computed, it will compute the inverse store it.
 
+###############################################################################################################
 
-makeCacheMatrix <- function(x = matrix()) {
-        ## This function takes a matrix as an argument.
-        ## Initiially the inverse ("inv") is set to NULL
-        
+makeVector <- function( x = matrix() ) {
+        ## This function takes a matrix as an argument and returns a list of functions for that matrix.
+	
+        ## Initially the inverse (stored in the variable "inv") is set to NULL as no value has been computed.
         inv <- NULL
                 
-                ## The 'set' function stores the matrix 'y' in varialbe 'x'
-                ## Note that 'x' is in a different environment than the 'set'
-                ## function, and hence the use of the <<- assignment operator.
-                ## After the matix 'x' is stored, the variable 'inv' is
-                ## set to NULL, which removes any previous matrix inverse.
-                ## This means that every time a new matrix is 'set', the
-                ## inverse is removed, even if the matrix is the same. This
-                ## prevents any misuse of the inverted matrix after a the 
-                ## 'set' function is called. Essentially the cache is cleared.
-                set <- function(y) {
+                ## The 'set' function stores the matrix 'y' in variable 'x'.
+                ## Note that 'x' is in a different environment than the 'set' function,
+		## requiring use of the <<- assignment operator.	
+                ## After matrix 'y' is stored in the variable "x",
+		## the variable 'inv' is set to NULL, removing any previous value (i.e. the cache is cleared).
+                set <- function( y ) {
                         x <<- y
                         inv <<- NULL
                 }
                 
-                ## The 'get' function returns the value of the matix.
-                ## It should be noted that the 'set' function must be called
-                ## before any use of the 'get' function; otherwise
-                ## an error will result.
+                ## The 'get' function returns the value of the matrix "x" (stored by the "set" function).
                 get <- function() x
                 
-                ## The 'setinv' function stores the value of the inverse
-                ## into the variable 'inv'.
-                setinv <- function(inverse) inv <<- inverse
+                ## The 'setinv' function assigns the value of the inverse to the variable 'inv'.
+                setinv <- function( inverse ) inv <<- inverse
                 
-                ## The 'getinv' function returns the value of the inverse that was
-                ## set with the 'setinv' function
+                ## The 'getinv' function returns the value of the inverse previously stored in variable "inv".
                 getinv <- function() inv
                 
-                ## The call to the 'makeCacheMatrix' returns this list, which is
-                ## a list of functions inside of 'makeCacheMatrix'
-                list(set = set, get = get,
-                     setinv = setinv,
-                     getinv = getinv)
+                ## Finally, the function 'makeVector' returns this list of functions.
+                list( set = set, get = get, setinv = setinv, getinv = getinv )
         }
-}
 
+###############################################################################################################
 
-## The 'cacheSolve' function can be called to return the inverse of a matix 'x'
-## When called, the function first retrieves the value of the inverse using the
-## 'getinv' function on the matix 'x. If the inverse has been calculated,
-## a non-NULL value is returned and then the cached inverse in the variable 'inv'
-## is returned.
+## The 'cacheSolve' function returns the inverse of a matrix 'x'. 
+## It first checks to see if the inverse has previously been calculated (indicated by a non-NULL value of the variable "inv").
+## If so, it retrieves the inverse from cache using the getinv' function on the matrix 'x. 
 
-## However, if 'getinv' returns a NULL value, then the matrix 'x' is retrieved
-## and the inverse is computed using the R 'solve' method. This inverse is then
-## stored for future use using the 'setinv' function.
+## However, if 'getinv' returns a NULL value (indicating no inverse is available in cache), 
+## then the matrix 'x' is retrieved, the inverse is computed using the R 'solve' function,
+## and then stored for future use using the 'setinv' function.
 
-cacheSolve <- function(x, ...) {
-## Returns a matrix that is the inverse of 'x'
+cacheSolve <- function( x, ... ) {
+## Returns the inverse of matrix 'x'.
+## If the inverse is not in cache, it is computed and stored for future use.
         
-        ## 'get' the inverse for matrix 'x'
+        ## Retrieve the inverse for matrix 'x'
         inv <- x$getinv()
-        
-        ## Test to see if the inverse is non-NULL, if TRUE, then return
-        ## the retrieved value 'inv' which is cached. Then exit the function
-        ## via the return command.
-        if(!is.null(inv)) {
-                message("getting cached data for inverse")
-                return(inv)
+	
+        ## Test to see if the inverse is non-NULL. If not NULL, retrieve the value of the inverse from cache.
+	## Then exit the function via the return() function.
+        if( !is.null( inv ) ) {
+                message( "Retrieving the inverse from cached data." )
+                return( inv )
         }
         
-        ## If the inverse is NULL, then this branch will be executed.
-        ## This part of the function gets the vector 'x' and computes the inverse
-        ## using the R 'solve' function. It then stores the inverse using the
-        ## 'setinv' function so that it is available for future use.
+        ## If the "inv" value is NULL, it indicates that no inverse has been computed. 
+	## In that case, this section retrieves the matrix 'x', computes the inverse using the R 'solve' function,
+	## and then stores the inverse using the setinv' function so that it is available for future use.
+	## The value of the inverse is then returned.
         data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
+        inv <- solve( data, ... )
+        x$setinv( inv )
         inv
 }
